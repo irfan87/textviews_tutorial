74b8aaf426f8c92b21e6a73c8fc89cb78aef8833
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-06-06 23:47:59 +0800

    app counter

--- app/src/main/java/com/irfanshukri203/textviewstutorial/MainActivity.kt
+++ app/src/main/java/com/irfanshukri203/textviewstutorial/MainActivity.kt
@@ -2,10 +2,19 @@ package com.irfanshukri203.textviewstutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var count = 0
+
+        btnCount.setOnClickListener{
+            count++
+
+            tvCount.text = "let's count the number together: $count"
+        }
     }
 }
